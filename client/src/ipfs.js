14399ebf93c97c898b48ee010045369baf6a1448
Ashwin <gadveashwin@gmail.com>
2024-02-25 22:08:14 +0530

    Change in comments

--- client/src/ipfs.js
+++ client/src/ipfs.js
@@ -3,7 +3,8 @@ const IPFS = require('ipfs-http-client');
 //Use this for localhost
 const ipfs = new IPFS({ host: 'localhost', port: 5001, protocol: 'http' });
 
-// Use this to use ipfs in network(Uncomment below line and comment above line and also configure config file in .ipfs folder)
+// Use this to use ipfs in network(Uncomment below line and comment above line 
+// and also configure .ipfs/config file)
 // const ipfs = new IPFS({ host: 'host_address', port: 5001, protocol: 'http' });
 
 export default ipfs;
